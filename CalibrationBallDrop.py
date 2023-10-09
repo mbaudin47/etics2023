5665671616445486c92e659766a68933d87c19ee
Michael BAUDIN <michael.baudin@gmail.com>
2023-10-09 12:26:35 +0200

    Fix data

--- CalibrationBallDrop.py
+++ CalibrationBallDrop.py
@@ -4,8 +4,63 @@ import openturns.viewer as otv
 import matplotlib.pyplot as plt
 
 # %%
+def printInterval(interval, indentation="   "):
+    """
+    Print the [g, h0] C.I. with readable units.
+    """
+    lowerBound = interval.getLowerBound()
+    upperBound = interval.getUpperBound()
+    print(
+        indentation,
+        "g in [%.3f, %.3f]" % (lowerBound[0] , upperBound[0] ),
+    )
+    print(
+        indentation,
+        "h0 in [%.2f, %.2f]" % (lowerBound[1], upperBound[1] ),
+    )
+    return None
+
+# %%
+def plotDistributionGridPDF(distribution):
+    """
+    Plot the marginal and bi-dimensional iso-PDF on a grid.
+
+    Parameters
+    ----------
+    distribution : ot.Distribution
+        The distribution.
+
+    Returns
+    -------
+    grid : ot.GridLayout(dimension, dimension)
+        The grid of plots.
+
+    """
+    dimension = distribution.getDimension()
+    grid = ot.GridLayout(dimension, dimension)
+    for i in range(dimension):
+        for j in range(dimension):
+            if i == j:
+                distributionI = distribution.getMarginal([i])
+                graph = distributionI.drawPDF()
+            else:
+                distributionJI = distribution.getMarginal([j, i])
+                graph = distributionJI.drawPDF()
+            graph.setLegends([""])
+            graph.setTitle("")
+            if i < dimension - 1:
+                graph.setXTitle("")
+            if j > 0:
+                graph.setYTitle("")
+            grid.setGraph(i, j, graph)
+    grid.setTitle("Iso-PDF values")
+    return grid
+
+# %%
 data = ot.Sample.ImportFromCSVFile("Ball_drops_data.csv")
-data[:5]
+drop1 = data[0:51]
+drop2 = data[51:]
+drop1[:5]
 
 # %%
 timeSample = data[:, 1]
@@ -91,22 +146,6 @@ view = otv.View(
 plt.subplots_adjust(right=0.8)
 
 # %%
-def printInterval(interval, indentation="   "):
-    """
-    Print the [g, h0] C.I. with readable units.
-    """
-    lowerBound = interval.getLowerBound()
-    upperBound = interval.getUpperBound()
-    print(
-        indentation,
-        "g in [%.3f, %.3f]" % (lowerBound[0] , upperBound[0] ),
-    )
-    print(
-        indentation,
-        "h0 in [%.2f, %.2f]" % (lowerBound[1], upperBound[1] ),
-    )
-    return None
-# %%
 thetaPosterior = calibrationResult.getParameterPosterior()
 interval = thetaPosterior.computeBilateralConfidenceIntervalWithMarginalProbability(
     0.95
@@ -114,41 +153,6 @@ interval = thetaPosterior.computeBilateralConfidenceIntervalWithMarginalProbabil
 print("95% C.I.:")
 printInterval(interval)
 
-# %%
-def plotDistributionGridPDF(distribution):
-    """
-    Plot the marginal and bi-dimensional iso-PDF on a grid.
-
-    Parameters
-    ----------
-    distribution : ot.Distribution
-        The distribution.
-
-    Returns
-    -------
-    grid : ot.GridLayout(dimension, dimension)
-        The grid of plots.
-
-    """
-    dimension = distribution.getDimension()
-    grid = ot.GridLayout(dimension, dimension)
-    for i in range(dimension):
-        for j in range(dimension):
-            if i == j:
-                distributionI = distribution.getMarginal([i])
-                graph = distributionI.drawPDF()
-            else:
-                distributionJI = distribution.getMarginal([j, i])
-                graph = distributionJI.drawPDF()
-            graph.setLegends([""])
-            graph.setTitle("")
-            if i < dimension - 1:
-                graph.setXTitle("")
-            if j > 0:
-                graph.setYTitle("")
-            grid.setGraph(i, j, graph)
-    grid.setTitle("Iso-PDF values")
-    return grid
 
 # %%
 grid = plotDistributionGridPDF(thetaPosterior)
@@ -162,3 +166,47 @@ plt.subplots_adjust(wspace=plot_space, hspace=plot_space)
 
 # %%
 print("+ Gaussian calibration")
+sigmaHeight = 1.e-1  # (m)
+
+# %%
+errorCovariance = ot.CovarianceMatrix(1)
+errorCovariance[0, 0] = sigmaHeight ** 2
+
+# %%
+sigmaG = 1
+sigmaH0 = 5
+covarianceMatrix = ot.CovarianceMatrix(2)
+covarianceMatrix[0, 0] = sigmaG ** 2
+covarianceMatrix[1, 1] = sigmaH0 ** 2
+print("Prior covariance matrix")
+print(covarianceMatrix)
+
+# %%
+algo = ot.GaussianLinearCalibration(
+    dropBallModelParametric, timeSample, heightSample, thetaInitial, 
+    covarianceMatrix, errorCovariance
+)
+algo.run()
+calibrationResult = algo.getResult()
+print("MAP =", thetaMAP)
+thetaPosterior = calibrationResult.getParameterPosterior()
+interval = thetaPosterior.computeBilateralConfidenceIntervalWithMarginalProbability(
+    0.95
+)[0]
+print("95% C.I.:")
+printInterval(interval)
+
+# %%
+graph = calibrationResult.drawObservationsVsPredictions()
+view = otv.View(graph)
+
+# %%
+graph = calibrationResult.drawParameterDistributions()
+view = otv.View(
+    graph,
+    figure_kw={"figsize": (10.0, 4.0)},
+    legend_kw={"bbox_to_anchor": (1.0, 1.0), "loc": "upper left"},
+)
+plt.subplots_adjust(right=0.8)
+
+# %%
