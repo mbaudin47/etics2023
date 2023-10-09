8587a6bfeceaae17e3a1327d1086dc7801f4c91e
Michael BAUDIN <michael.baudin@gmail.com>
2023-10-09 11:47:02 +0200

    Ajoute le modèle. Corrige l'en-tête du CSV

--- CalibrationBallDrop.py
+++ CalibrationBallDrop.py
@@ -6,3 +6,10 @@ data = ot.Sample.ImportFromCSVFile("Ball_drops_data.csv")
 data[:5]
 
 # %%
+def dropBallModel(x):
+    g = x[0]
+    h0 = x[1]
+    t = x[2]
+    h = -g * 0.5 * t ** 2 + h0
+    h = max(0.0, h)
+    return h
