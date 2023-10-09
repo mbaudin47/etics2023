75539576ef6c247025c42f9e420683360a3369d9
Michael BAUDIN <michael.baudin@gmail.com>
2023-10-09 12:15:37 +0200

    Finished NLLS analysis

--- CalibrationBallDrop.py
+++ CalibrationBallDrop.py
@@ -1,6 +1,7 @@
 # %%
 import openturns as ot
 import openturns.viewer as otv
+import matplotlib.pyplot as plt
 
 # %%
 data = ot.Sample.ImportFromCSVFile("Ball_drops_data.csv")
@@ -54,13 +55,110 @@ graph.setLegendPosition("topright")
 view = otv.View(graph)
 
 # %%
-# Calibration with non linear least squares
+print("+ Calibration with non linear least squares")
 algo = ot.NonLinearLeastSquaresCalibration(
     dropBallModelParametric, timeSample, heightSample, thetaInitial
     )
 algo.run()
 calibrationResult = algo.getResult()
 thetaMAP = calibrationResult.getParameterMAP()
-print(thetaMAP)
+print("MAP =", thetaMAP)
 
 # %%
+graph = calibrationResult.drawObservationsVsInputs()
+aEstimated, bEstimated = thetaMAP
+title = "After calibration : a = %.4f, b = %.4f" % (aEstimated, bEstimated)
+graph.setTitle(title)
+graph.setLegendPosition("topright")
+view = otv.View(graph)
+
+# %%
+graph = calibrationResult.drawResiduals()
+graph.setLegendPosition("topright")
+view = otv.View(graph)
+
+# %%
+graph = calibrationResult.drawResidualsNormalPlot()
+view = otv.View(graph)
+
+# %%
+graph = calibrationResult.drawParameterDistributions()
+view = otv.View(
+    graph,
+    figure_kw={"figsize": (10.0, 4.0)},
+    legend_kw={"bbox_to_anchor": (1.0, 1.0), "loc": "upper left"},
+)
+plt.subplots_adjust(right=0.8)
+
+# %%
+def printInterval(interval, indentation="   "):
+    """
+    Print the [g, h0] C.I. with readable units.
+    """
+    lowerBound = interval.getLowerBound()
+    upperBound = interval.getUpperBound()
+    print(
+        indentation,
+        "g in [%.3f, %.3f]" % (lowerBound[0] , upperBound[0] ),
+    )
+    print(
+        indentation,
+        "h0 in [%.2f, %.2f]" % (lowerBound[1], upperBound[1] ),
+    )
+    return None
+# %%
+thetaPosterior = calibrationResult.getParameterPosterior()
+interval = thetaPosterior.computeBilateralConfidenceIntervalWithMarginalProbability(
+    0.95
+)[0]
+print("95% C.I.:")
+printInterval(interval)
+
+# %%
+def plotDistributionGridPDF(distribution):
+    """
+    Plot the marginal and bi-dimensional iso-PDF on a grid.
+
+    Parameters
+    ----------
+    distribution : ot.Distribution
+        The distribution.
+
+    Returns
+    -------
+    grid : ot.GridLayout(dimension, dimension)
+        The grid of plots.
+
+    """
+    dimension = distribution.getDimension()
+    grid = ot.GridLayout(dimension, dimension)
+    for i in range(dimension):
+        for j in range(dimension):
+            if i == j:
+                distributionI = distribution.getMarginal([i])
+                graph = distributionI.drawPDF()
+            else:
+                distributionJI = distribution.getMarginal([j, i])
+                graph = distributionJI.drawPDF()
+            graph.setLegends([""])
+            graph.setTitle("")
+            if i < dimension - 1:
+                graph.setXTitle("")
+            if j > 0:
+                graph.setYTitle("")
+            grid.setGraph(i, j, graph)
+    grid.setTitle("Iso-PDF values")
+    return grid
+
+# %%
+grid = plotDistributionGridPDF(thetaPosterior)
+view = otv.View(
+    grid,
+    figure_kw={"figsize": (6.0, 6.0)},
+    legend_kw={"bbox_to_anchor": (1.0, 1.0), "loc": "upper left"},
+)
+plot_space = 0.5
+plt.subplots_adjust(wspace=plot_space, hspace=plot_space)
+
+# %%
+print("+ Gaussian calibration")
